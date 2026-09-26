cbff8f8a07780ff6d68df9523c230e4876d6f54a
Yogaditya <yogaditya.giduturi@gmail.com>
2026-09-26 13:44:34 +0530

    Add initial exploration script and gitignore

--- business_entity_resolution/src/explore.py
+++ business_entity_resolution/src/explore.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+s1 = pd.read_csv("student_resource/dataset/train/train_source1.tsv", sep="\t", nrows=1000)
+gt = pd.read_csv("student_resource/dataset/train/train_ground_truth.tsv", sep="\t", nrows=1000)
+
+print(s1.shape)
+print(s1.head())
+print(gt.head())
